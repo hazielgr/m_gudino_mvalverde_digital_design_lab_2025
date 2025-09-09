3fe124fee02f14a941924557f7cda80e370b2e56
Michael Valverde <maicolvn1399@gmail.com>
2025-09-09 15:27:29 -0600

    [Update] AND, OR, XOR gates implemented from scratch for ALU

---
+++ Laboratorio_2/Problema_1/or1.sv
@@ -0,0 +1,11 @@
+module or1(
+  input  logic a, b,
+  output logic y
+);
+  always_comb begin
+    unique case ({a,b})
+      2'b00:               y = 1'b0;
+      2'b01, 2'b10, 2'b11: y = 1'b1;
+    endcase
+  end
+endmodule
